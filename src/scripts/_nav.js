6b8e28aa6c97324560b418adaf480ebd6cf9ca64
Jason Maurer <j@maur.co>
2015-12-12 13:59:55 -0700

    reorganizing styles

--- src/scripts/_nav.js
+++ src/scripts/_nav.js
@@ -1,63 +1,48 @@
-var nav = $('header .nav');
-// var IS_MOBILE = false;
-// var KEEP_CLOSED = false;
-//
-var menu = nav.children('.menu');
-// if (menu.css('top') === '0px') IS_MOBILE = true && nav.addClass('hidden');
-// if (nav.hasClass('keep-closed')) KEEP_CLOSED = true;
-//
-// /* open and close menu at the waypoint */
-// new Waypoint({ // Waypoint is defined by CDN script in layout.jade
-//   element: $('.heading h4').get(0),
-//   handler: function(dir) {
-//     if (KEEP_CLOSED) return;
-//
-//     if (dir === 'down') nav.addClass('hidden')
-//       else nav.removeClass('hidden')
-//   }
-// })
+var nav = $('nav')
+var menu = $('#nav-menu')
+var piglet = $('#piglet')
 
 /* open and close menu when icon is clicked */
-menu.on('click', function() {
-  nav.removeClass('hidden')
-  // if (IS_MOBILE) nav.addClass('force-open')
+piglet.on('click', function() {
+  nav.addClass('opened')
 
-  /* amount of pixels (up or down) to trigger the menu to close */
-  var TRIGGER_AT = 70
-
-  /* auto-closes menu when user scrolls up or down */
   var w = $(window)
   var origTop = w.scrollTop()
+  var difference
+
+  /* auto-closes menu when user scrolls up or down */
   w.on('scroll', function(e) {
     var scrollTop = $(e.target).scrollTop()
-    var difference = Math.abs(scrollTop - origTop)
 
-    if (difference >= TRIGGER_AT) {
-      nav.addClass('hidden').removeClass('force-open')
-      // w.off('scroll')
+    difference = Math.abs(scrollTop - origTop)
+
+    /* amount of pixels (up or down) to trigger the menu to close */
+    if (difference >= 70) {
+      nav.removeClass('opened')
+      difference = null
     }
   })
 })
 
 /* change menu icon color based on section */
-if ($('.nav > .menu').hasClass('alternate-menu')) {
-  var darkSections = document.getElementsByTagName('section')
-  for (var i = 0; i < darkSections.length; i++) {
-    new Waypoint({
-      element: darkSections[i],
-      offset: 55,
-      handler: function(dir) {
-        var el = $($(this).get(0).element)
-
-        if (dir === 'down' && el.hasClass('dark'))
-          menu.removeClass('black').addClass('white')
-        else if (dir === 'up' && el.hasClass('dark'))
-          menu.removeClass('white').addClass('black')
-        else if (dir === 'down' && !el.hasClass('dark'))
-          menu.removeClass('white').addClass('black')
-        else if (dir === 'up' && !el.hasClass('dark'))
-          menu.removeClass('black').addClass('white')
-      }
-    })
-  }
-}
+// if ($('.nav > .menu').hasClass('alternate-menu')) {
+//   var darkSections = document.getElementsByTagName('section')
+//   for (var i = 0; i < darkSections.length; i++) {
+//     new Waypoint({
+//       element: darkSections[i],
+//       offset: 55,
+//       handler: function(dir) {
+//         var el = $($(this).get(0).element)
+//
+//         if (dir === 'down' && el.hasClass('dark'))
+//           menu.removeClass('black').addClass('white')
+//         else if (dir === 'up' && el.hasClass('dark'))
+//           menu.removeClass('white').addClass('black')
+//         else if (dir === 'down' && !el.hasClass('dark'))
+//           menu.removeClass('white').addClass('black')
+//         else if (dir === 'up' && !el.hasClass('dark'))
+//           menu.removeClass('black').addClass('white')
+//       }
+//     })
+//   }
+// }
