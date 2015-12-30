a5f5ba294dee193ec5ab731926147eeb36149eb2
Matt Sutherland <mattwsutherland@gmail.com>
2015-12-29 19:36:09 -0700

    text changes, new work page

--- _src/scripts/_jsonp.js
+++
@@ -1,6 +0,0 @@
-/* for injecting a json-p file */
-$('*[data-jsonp]').on('click', function() {
-  var script = document.createElement('SCRIPT')
-  script.src = $(this).attr('data-jsonp')
-  document.body.appendChild(script)
-})
