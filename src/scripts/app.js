8e77dd0d7d747f6faf6a95d1509bd5448b8e0938
Jason Maurer <j@maur.co>
2016-03-29 12:16:21 -0600

    some optimizations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -18,3 +18,15 @@ $(document).ready(function() {
     dots: true
   })
 })
+
+var body = document.body, timer
+window.addEventListener('scroll', function() {
+  clearTimeout(timer)
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  }, 300)
+}, false)
