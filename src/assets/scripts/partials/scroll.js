089fb50054f1b5f53afd211a011acf8d310c0487
Nhat Nguyen <nhat@thepixelme.com>
2017-03-15 19:45:19 -0600

    updated some images and adjusted skrollr parallax

--- src/assets/scripts/partials/scroll.js
+++ src/assets/scripts/partials/scroll.js
@@ -1,7 +1,6 @@
 // get skrollr
 var skrollr = require('skrollr');
 
-// activate skrollr when width is below 500px
 if ($(window).width() > 500) {
   var s = skrollr.init({
     smoothScrolling: false,
