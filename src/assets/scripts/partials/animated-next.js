2d43b544d174f5508210305c18a58847a8d1dda3
Nhat Nguyen <nhat@thepixelme.com>
2017-05-16 11:17:27 -0600

    added animation to nextup area

--- src/assets/scripts/partials/animated-next.js
+++ src/assets/scripts/partials/animated-next.js
@@ -1,43 +1,43 @@
-// function animatethis(targetElement, speed) {
-//     $(targetElement).animate({right: "-=50px"},
-//     {
-//         duration: speed,
-//         complete: function ()
-//         {
-//             targetElement.animate({ right: "+=50px" },
-//             {
-//                 duration: speed,
-//                 complete: function ()
-//                 {
-//                     animatethis(targetElement, speed);
-//                 }
-//             });
-//         }
-//     });
-// };
-//
-// animatethis($('.animated-next-border-one'), 3000);
-//
-//
-// function animatethisToo(targetElement, speed) {
-//     $(targetElement).animate({ top: "-=50px"},
-//     {
-//         duration: speed,
-//         complete: function ()
-//         {
-//             targetElement.animate({ top: "+=50px" },
-//             {
-//                 duration: speed,
-//                 complete: function ()
-//                 {
-//                     animatethisToo(targetElement, speed);
-//                 }
-//             });
-//         }
-//     });
-// };
-//
-// animatethisToo($('.animated-next-border-two'), 3000);
+function animatethis(targetElement, speed) {
+    $(targetElement).animate({right: "-=50px"},
+    {
+        duration: speed,
+        complete: function ()
+        {
+            targetElement.animate({ right: "+=50px" },
+            {
+                duration: speed,
+                complete: function ()
+                {
+                    animatethis(targetElement, speed);
+                }
+            });
+        }
+    });
+};
+
+animatethis($('.animated-next-border-one'), 3000);
+
+
+function animatethisToo(targetElement, speed) {
+    $(targetElement).animate({ top: "-=50px"},
+    {
+        duration: speed,
+        complete: function ()
+        {
+            targetElement.animate({ top: "+=50px" },
+            {
+                duration: speed,
+                complete: function ()
+                {
+                    animatethisToo(targetElement, speed);
+                }
+            });
+        }
+    });
+};
+
+animatethisToo($('.animated-next-border-two'), 3000);
 
 
 // $(".animated-next-content").on("mouseover", function () {
